e994b2e1ab1fc0247cff22b34873e91cdd45a122
Phillipe Sobrinho <phillipe_sobrinho@hotmail.com>
2024-03-04 23:57:19 +0000

    update

--- script.js
+++ script.js
@@ -13,4 +13,33 @@ const jump = () => {
     }, 500);   
 }
 
-document.addEventListener("keydown" , jump);
+    
+const loop = setInterval(() => {
+console.log("loop")
+
+const pipePosition = pipe.offleft;
+const marioPosition = +window.getComputedStyle(mario). bottom.replace("px, ");
+
+console.log(marioPosition)
+
+if (pipePosition <= 1950 && pipePosition > 0 && marioPosition < 1950) {
+
+pipe.style.animation = "none";
+pipe.style.left= `${pipePosition}px`;
+ 
+mario.style.animation = "none";
+mario.style.bottom = `${marioPosition}px`;
+
+
+mario.src = "/imagem/game-over.png";
+mario.style.width = "75px"
+mario.style.marginleft = "60px"
+
+clearInterval(loop);
+}
+
+}, 10); 
+
+
+document.addEventListener("keydown" , jump);  
+
