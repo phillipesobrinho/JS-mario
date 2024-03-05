566e8949a6e32c9201774821cf5b81c59bdbe0b1
Phillipe Sobrinho <phillipe_sobrinho@hotmail.com>
2024-03-05 16:19:02 +0000

    js ajustments

--- script.js
+++ script.js
@@ -1,47 +1,36 @@
 const pipe = document.querySelector(".pipe");
 const mario = document.querySelector(".mario");
 
-
 const jump = () => {
     mario.classList.add("jump");
 
-
     setTimeout(() => {
-
         mario.classList.remove("jump");
-
-    }, 500);   
+    }, 500);
 }
 
-document.addEventListener("keydown" , jump);  
+const checkCollision = () => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
 
-/*
-const loop = setInterval(() => {
-console.log("loop")
+    if (pipePosition <= 460 && pipePosition > 0 && marioPosition < 10) {
+        pipe.style.animation = "none";
+        pipe.style.left = `${pipePosition}px`;
 
-const pipePosition = pipe.offleft;
-const marioPosition = +window.getComputedStyle(mario). bottom.replace("px, ");
+        mario.style.animation = "none";
+        mario.style.bottom = `${marioPosition}px`;
+        
+        mario.src = "/Imagens/game-over.png";
+        mario.style.width = "95px";
+        mario.style.marginLeft = "55px";
 
-console.log(marioPosition)
+        clearInterval(loop);
+    }
 
-if (pipePosition == 1950 && pipePosition > 0 && marioPosition < 1950) {
-
-pipe.style.animation = "none";
-pipe.style.left= `${pipePosition}px`;
- 
-mario.style.animation = "none";
-mario.style.bottom = `${marioPosition}px`;
-
-
-mario.src = "/imagem/game-over.png";
-mario.style.width = "75px"
-mario.style.marginleft = "60px"
-
-clearInterval(loop);
+    requestAnimationFrame(checkCollision);
 }
 
-}, 10); 
+// Iniciar a verificação de colisão
+requestAnimationFrame(checkCollision);
 
-
-document.addEventListener("keydown" , jump);  
-*/
+document.addEventListener("keydown", jump);
